dc2cc5454603cd20c4203d253faf3ecd18075def
naveenkumar80 <naveenumar80032@gmail.com>
2025-12-15 13:50:11 +0530

    routes setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    
+    <Routes>  
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+    
   )
 }
 
